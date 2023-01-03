2c2a20d6b277145800323345509b468015c8a042
dineshkudva <dineshkudva12@gmail.com>
2023-01-03 17:51:37 +0530

    initial commit

---
+++ src/CharComparator.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class CharComparator implements Comparator<TreeNode> {
+
+    @Override
+    public int compare(TreeNode o1, TreeNode o2) {
+        // TODO Auto-generated method stub
+
+
+        if(o1.getFreq() == o2.getFreq()) {
+
+            if((int)o1.getChar()>(int)o2.getChar())
+                return 1;
+            else
+                return -1;
+        }
+
+        return o1.getFreq() - o2.getFreq();
+    }
+
+}
---
+++ src/TreeNode.java
@@ -0,0 +1,24 @@
+public class TreeNode {
+    private Character ch;
+    private Integer frequency;
+
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(char a,int b){
+
+        this.ch = a;
+        this.frequency = b;
+        this.left = null;
+        this.right = null;
+
+    }
+
+    public char getChar() {
+        return this.ch.charValue();
+    }
+
+    public int getFreq() {
+        return this.frequency;
+    }
+}
