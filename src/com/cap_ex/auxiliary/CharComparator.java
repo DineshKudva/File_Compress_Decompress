0a3aba801e6af2722fb2ca32af536fdcd7f2862c
dineshkudva <dineshkudva12@gmail.com>
2023-01-26 00:22:07 +0530

    Custom comparator updated

--- src/com/cap_ex/auxiliary/CharComparator.java
+++ src/com/cap_ex/auxiliary/CharComparator.java
@@ -1,23 +1,14 @@
 package com.cap_ex.auxiliary;
 
-import com.cap_ex.auxiliary.TreeNode;
-
 import java.util.*;
 
 public class CharComparator implements Comparator<TreeNode> {
 
     @Override
     public int compare(TreeNode o1, TreeNode o2) {
-        // TODO Auto-generated method stub
-
 
         if (o1.getFreq() == o2.getFreq()) {
-
-//            if ((int) o1.getChar() > (int) o2.getChar())
-//                return 1;
-//            else
-//                return -1;
-             return o1.getAscii()-o2.getAscii();
+            return o1.getAsciiVal() - o2.getAsciiVal();
         }
 
         return o1.getFreq() - o2.getFreq();
