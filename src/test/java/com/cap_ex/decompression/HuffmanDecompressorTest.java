a7c1569b40542d63d0ea04fc8b6fee78ff7400bf
Dinesh Kudva <dineshkudva12@gmail.com>
2023-02-06 17:16:28 +0530

    testcases to HuffmanDecompressorTest class added

--- src/test/java/com/cap_ex/decompression/HuffmanDecompressorTest.java
+++ src/test/java/com/cap_ex/decompression/HuffmanDecompressorTest.java
@@ -2,16 +2,36 @@ package com.cap_ex.decompression;
 
 import org.junit.Test;
 
+import java.io.File;
+
 import static org.junit.Assert.*;
 
 public class HuffmanDecompressorTest {
+    IHuffmanDecompress testRef = new HuffmanDecompressor();
+    File fileObj = new File("src/textFiles/compressedFiles/nonExistent.txt");
+    @Test(expected = RuntimeException.class)
+    public void testDecompressFail() {
+        testRef.decompress(fileObj);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testDecompressEmpty() {
+        fileObj = new File("src/textFiles/compressedFiles/empty.txt");
+        testRef.decompress(fileObj);
+    }
 
     @Test
-    public void decompress() {
+    public void testDecompress(){
+        fileObj = new File("src/textFiles/compressedFiles/nonExistent.txt");
+        String actual = testRef.decompress(fileObj);
+        String expected = "src/textFiles/decompressedFiles/decompressed.txt";
+
+        assertEquals(expected,actual);
     }
 
     @Test
     public void dataDecompression() {
+
     }
 
     @Test
