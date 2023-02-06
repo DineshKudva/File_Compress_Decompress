e9fd25144d485a1a880d43eebac3a56154613ccf
Dinesh Kudva <dineshkudva12@gmail.com>
2023-02-06 16:46:21 +0530

    test class for HuffmanDecompressor class created

---
+++ src/test/java/com/cap_ex/decompression/HuffmanDecompressorTest.java
@@ -0,0 +1,24 @@
+package com.cap_ex.decompression;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HuffmanDecompressorTest {
+
+    @Test
+    public void decompress() {
+    }
+
+    @Test
+    public void dataDecompression() {
+    }
+
+    @Test
+    public void deserialize() {
+    }
+
+    @Test
+    public void treeBuilder() {
+    }
+}
