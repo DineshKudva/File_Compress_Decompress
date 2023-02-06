b4e7c6d21addeade7ecffb4bd15cc2609a6281b8
Dinesh Kudva <dineshkudva12@gmail.com>
2023-02-06 15:42:15 +0530

    GeneralMethods test cases updated

--- src/test/java/com/cap_ex/auxiliary/GeneralMethodsTest.java
+++ src/test/java/com/cap_ex/auxiliary/GeneralMethodsTest.java
@@ -1,5 +1,6 @@
 package com.cap_ex.auxiliary;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.HashMap;
@@ -8,15 +9,22 @@ import java.util.Map;
 import static org.junit.Assert.*;
 
 public class GeneralMethodsTest {
+    TreeNode child1, child2, root;
+    IGeneralMethods testRef;
+    Map<Character, Integer> freqMap;
+    Map<Character, String> codeMap;
 
-    TreeNode child1 = new TreeNode('a',1,null,null);
-    TreeNode child2 = new TreeNode('b',2,null,null);
-    TreeNode root = new TreeNode('$',child1.getFreq()+child2.getFreq(),child1,child2);
 
-    Map<Character,Integer> freqMap = new HashMap<>();
-    Map<Character,String> codeMap = new HashMap<>();
+    @Before
+    public void setup() {
+        child1 = new TreeNode('a', 1, null, null);
+        child2 = new TreeNode('b', 2, null, null);
+        root = new TreeNode('$', child1.getFreq() + child2.getFreq(), child1, child2);
+        freqMap = new HashMap<>();
+        codeMap = new HashMap<>();
+        testRef = new GeneralMethods();
 
-    IGeneralMethods testRef = new GeneralMethods();
+    }
     @Test
     public void testGetAscii(){
         String input = "01100010";
@@ -41,31 +49,40 @@ public class GeneralMethodsTest {
         assertEquals("check obtained binary string (from an integer)",expected,actual);
     }
 
+    @Test(expected = ArithmeticException.class)
+    public void getBinaryFromIntNegative() throws Exception{
+        int input = -20;
+        String actual = testRef.getBinaryFromInt(input);
+    }
+
+    @Test
+    public void testSerializeForNull(){
+        String actual = testRef.serialize(null);
+        assertEquals(null,actual);
+    }
     @Test
     public void testSerialize() {
         String actual = testRef.serialize(root);
         String expected = "$,97,#,#,98,#,#";
         assertEquals(expected,actual);
+    }
 
-        actual = testRef.serialize(null);
-        expected = null;
-        assertEquals(null,null);
+    @Test
+    public void testGetArraySizeZero(){
+        int actaul = testRef.getArraySize(codeMap,freqMap);
+        assertEquals(0,actaul);
     }
 
     @Test
     public void testGetArraySize(){
-        int actaul = testRef.getArraySize(codeMap,freqMap);
-        int expected = 0;
-        assertEquals(expected,actaul);
-
         freqMap.put('a',2);
         freqMap.put('b',3);
 
         codeMap.put('a',"0");
         codeMap.put('b',"1");
 
-        actaul = testRef.getArraySize(codeMap,freqMap);
-        expected = 1;
+        int actaul = testRef.getArraySize(codeMap,freqMap);
+        int expected = 1;
         assertEquals(expected, actaul);
     }
 }
