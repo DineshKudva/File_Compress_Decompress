2d0ef2d42b3140827921d75ba8000b9eb0ba564f
Dinesh Kudva <dineshkudva12@gmail.com>
2023-02-11 11:05:25 +0530

    String return format changed to StringBuilder

--- src/main/java/com/cap_ex/File_handle/FileHandler.java
+++ src/main/java/com/cap_ex/File_handle/FileHandler.java
@@ -1,23 +1,27 @@
 package com.cap_ex.File_handle;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class FileHandler implements IData_Handle {
 
     File fileObj;
 
-    public FileHandler(String source){
+    public FileHandler(String source) {
         fileObj = new File(source);
     }
+
     @Override
-    public String readContent() {
+    public StringBuilder readContent() {
         StringBuilder result = new StringBuilder();
 
         try {
             FileReader fileReader = new FileReader(fileObj);
             int val = fileReader.read();
 
-            while(val != -1){
+            while (val != -1) {
                 char ch = (char) val;
                 result.append(ch);
                 val = fileReader.read();
@@ -31,6 +35,6 @@ public class FileHandler implements IData_Handle {
             throw new RuntimeException(e);
         }
 
-        return result.toString();
+        return result;
     }
 }
