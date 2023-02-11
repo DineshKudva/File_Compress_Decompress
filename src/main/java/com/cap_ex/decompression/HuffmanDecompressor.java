ab0e054cb19cd9df6b7dd6d9f6fc93bccfd5adf3
Dinesh Kudva <dineshkudva12@gmail.com>
2023-02-11 11:06:56 +0530

    extractContents method removed from Decompression side to IGeneralMethods interface

--- src/main/java/com/cap_ex/decompression/HuffmanDecompressor.java
+++ src/main/java/com/cap_ex/decompression/HuffmanDecompressor.java
@@ -1,10 +1,13 @@
 package com.cap_ex.decompression;
 
+import com.cap_ex.auxiliary.GeneralMethods;
+import com.cap_ex.auxiliary.IGeneralMethods;
 import com.cap_ex.auxiliary.TreeNode;
-import com.cap_ex.auxiliary.*;
 
-import java.io.*;
-import java.util.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class HuffmanDecompressor implements IHuffmanDecompress {
 
@@ -12,43 +15,16 @@ public class HuffmanDecompressor implements IHuffmanDecompress {
     Scanner fileName = new Scanner(System.in);
     public int nodeListIdx;
 
-    public HuffmanDecompressor(){
+    public HuffmanDecompressor() {
         nodeListIdx = 0;
         method = new GeneralMethods();
     }
 
-    public HuffmanDecompressor(IGeneralMethods method){
+    public HuffmanDecompressor(IGeneralMethods method) {
         nodeListIdx = 0;
         this.method = method;
     }
 
-    @Override
-    public IFileContents extractContents(File fileObj) {
-
-        IFileContents fileContents;
-
-        try {
-
-            FileInputStream fin = new FileInputStream(fileObj);
-            ObjectInputStream obj = new ObjectInputStream(fin);
-
-            fileContents = new FileContents();
-
-            fileContents.setHuffTree((String)obj.readObject());
-            fileContents.setExtraBits((int) obj.readInt());
-            fileContents.setByteArray((byte[]) obj.readObject());
-
-            obj.close();
-            fin.close();
-
-
-        } catch (IOException | ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-
-        return fileContents;
-
-    }
 
     @Override
     public String getBinaryData(byte[] byteArray) {
@@ -56,11 +32,11 @@ public class HuffmanDecompressor implements IHuffmanDecompress {
 
         int val;
 
-        for(byte myByte:byteArray){
-            val = (int) myByte;
+        for (byte myByte : byteArray) {
+            val = myByte;
 
-            if(val<0)
-                val = val+256;
+            if (val < 0)
+                val = val + 256;
 
             String binaryEqui = method.getBinaryFromInt(val);
 
@@ -82,12 +58,12 @@ public class HuffmanDecompressor implements IHuffmanDecompress {
         int length = binaryCode.length() - extraBits;
         TreeNode temp = root;
 
-        for(int i=0;i<length;i++) {
+        for (int i = 0; i < length; i++) {
             char ch = binaryCode.charAt(i);
 
             if (ch == '1' && temp.getRightChild() != null)
                 temp = temp.getRightChild();
-            else if(ch == '0' && temp.getLeftChild() != null)
+            else if (ch == '0' && temp.getLeftChild() != null)
                 temp = temp.getLeftChild();
 
 
@@ -106,41 +82,41 @@ public class HuffmanDecompressor implements IHuffmanDecompress {
             throw new RuntimeException(e);
         }
 
-        System.out.println("Size of decompressed file:"+newFile.length()/1024+" Kb");
+        System.out.println("Size of decompressed file:" + newFile.length() / 1024 + " Kb");
 
         return resultFilePath;
     }
 
-    public TreeNode deserialize(String nodeList){
-        if(nodeList == null || nodeList.length() == 0)
+    public TreeNode deserialize(String nodeList) {
+        if (nodeList == null || nodeList.length() == 0)
             return null;
-       nodeListIdx= 0;
-       String[] arr = nodeList.split(",");
+        nodeListIdx = 0;
+        String[] arr = nodeList.split(",");
 
-       TreeNode root = null;
-       if(arr.length!=0)
-        root = this.treeBuilder(arr);
+        TreeNode root = null;
+        if (arr.length != 0)
+            root = this.treeBuilder(arr);
 
         return root;
     }
 
-    public TreeNode treeBuilder(String[] arr){
-        if(arr[nodeListIdx].equals("#"))
+    public TreeNode treeBuilder(String[] arr) {
+        if (arr[nodeListIdx].equals("#"))
             return null;
 
         TreeNode root;
 
-        if(arr[nodeListIdx].equals("$"))
-            root = new TreeNode(arr[nodeListIdx].charAt(0),0,null,null);
+        if (arr[nodeListIdx].equals("$"))
+            root = new TreeNode(arr[nodeListIdx].charAt(0), 0, null, null);
         else
-            root = new TreeNode((char)Integer.parseInt(arr[nodeListIdx]),0,null,null);
+            root = new TreeNode((char) Integer.parseInt(arr[nodeListIdx]), 0, null, null);
 
-       nodeListIdx++;
+        nodeListIdx++;
         root.setLeft(treeBuilder(arr));
-       nodeListIdx++;
-          root.setRight(treeBuilder(arr));
+        nodeListIdx++;
+        root.setRight(treeBuilder(arr));
 
-       return root;
+        return root;
     }
 
 
